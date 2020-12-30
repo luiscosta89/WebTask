fff8335118cfdd5d7dc5e89e6d4dec9b194fc5c6
Luis Antonio Costa <luis.leite89@gmail.com>
2020-12-30 00:23:49 -0300

    Added files
    Added project files but there is an error when creation the database.

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTask.Models; 
+
+namespace WebTask.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(UserContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var users = new User[]
+            {
+                new User{Name="Carson Alexander", Email="carson_alex@gmail.com", BirthDate="02/03/1992", City="Porto Alegre", State="RS"},
+                new User{Name="Meredith Alonso", Email="meredith_al@gmail.com", BirthDate="10/01/1991", City="Porto Alegre", State="RS"},
+                new User{Name="Arturo Anand", Email="arturo_an@gmail.com", BirthDate="15/03/1992", City="Porto Alegre", State="RS"},
+                new User{Name="Gytis Barzdukas", Email="gytis_barz@gmail.com", BirthDate="21/06/1995", City="Porto Alegre", State="RS"},
+                new User{Name="Yan Li", Email="yan_li@gmail.com", BirthDate="04/11/1994", City="Porto Alegre", State="RS"},
+                new User{Name="Peggy Justice", Email="peggy_jus@gmail.com", BirthDate="28/12/1991", City="Porto Alegre", State="RS"},
+                new User{Name="Laura Norman", Email="laura_nor@gmail.com", BirthDate="13/10/1996", City="Porto Alegre", State="RS"}
+            };
+            foreach (User u in users)
+            {
+                context.Users.Add(u);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTask.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string BirthDate { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }         
+    }
+}
